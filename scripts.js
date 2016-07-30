3d40fbffa5958569b79b28128008efd94f99bcef
Jared Stock <jstock529@gmail.com>
2016-07-30 19:30:44 -0400

    Set groundwork for quadratic interpolation.

--- scripts.js
+++ scripts.js
@@ -53,6 +53,37 @@ function GetPoints(x1,y1,x2,y2,x,y){
   chart.redraw()
 }
 
+// function Populate(){
+//   if (document.getElementsByName("type").value== "lin"){
+//     document.getElementById("quad_label").remove();
+//     document.getElementById("quadx").remove();
+//     document.getElementById("quady").remove();
+//   } else if (document.getElementById("type"=="quad").value){
+//
+//     var x = document.createElement("input");
+//     x.type = "text";
+//     x.setAttribute("id", "quadx");
+//     var lx = document.createTextNode("X Value");
+//     x.appendChild(lx);
+//     document.getElementById("quadrow").appendChild(x);
+//
+//     var y = document.createElement("input");
+//     y.type = "text";
+//     y.setAttribute("id", "quady");
+//
+//     var ly = document.createTextNode("X Value");
+//     y.appendChild(ly);
+//     document.getElementById("quadrow").appendChild(y);
+//
+//     var p = document.createElement("P");
+//     p.setAttribute("id", "quad_label");
+//     var l = document.createTextNode("Point 3");
+//     p.appendChild(x);
+//     p.appendChild(y);
+//     document.getElementById("quadrow").appendChild(p);
+//   }
+// }
+
 function Reset(){
   var chart = $('#container').highcharts();
   document.getElementById("x1").value= ""
@@ -67,51 +98,70 @@ function Reset(){
   }
 }
 
-function Interpolate() {
+function Interpolate(){
+  // if (document.getElementById("lin").value)
+  LinearInterpolate()
+  // } else if (document.getElementById("quad").value){
+  //   QuadraticInterpolate()
+  // }
+}
+function LinearInterpolate() {
   var x1=document.getElementById("x1").value
   var x2=document.getElementById("x2").value
   var y1=document.getElementById("y1").value
   var y2=document.getElementById("y2").value
   var x=document.getElementById("x").value
   var y=document.getElementById("y").value
-  if (x=="" && y==""){
-    //  Message here
+  if (x1=="" || y1=="" || x2=="" || y2==""){
+    alert("Enter a values for both known points.");
+    return
+  } else if (x=="" && y==""){
+    alert("Enter a value for the target x or y point.");
+    return
   } else if (x!="" && y!=""){
-    //  Message here
+    alert("Only enter a value for the X or Y target value, not both.");
+    return
   } else if (y == ""){
     y = parseFloat(y1) + parseFloat((x-x1)*((y2-y1)/(x2-x1)));
     y= y.toFixed(3)
     document.getElementById("result").innerHTML= y
-    document.getElementById("y").value= y
+    // document.getElementById("y").value= y
   } else if (x == ""){
     x=parseFloat(x1)+parseFloat((y-y1)*((x2-x1)/(y2-y1)));
     x=x.toFixed(3)
-    document.getElementById("x").value= x
+    // document.getElementById("x").value= x
     document.getElementById("result").innerHTML= x
   }
   GetPoints(x1,y1,x2,y2,x,y)
 }
 
-function QuadraticInterpolate() {
-  var x1=document.getElementById("x1").value
-  var x2=document.getElementById("x2").value
-  var y1=document.getElementById("y1").value
-  var y2=document.getElementById("y2").value
-  var x=document.getElementById("x").value
-  var y=document.getElementById("y").value
-  if (x=="" && y==""){
-    //  Message here
-  } else if (x!="" && y!=""){
-    //  Message here
-  } else if (y == ""){
-    y = parseFloat(y1) + parseFloat((x-x1)*((y2-y1)/(x2-x1)));
-    y= y.toFixed(3)
-    document.getElementById("result").innerHTML= y
-    document.getElementById("y").value= y
-  } else if (x == ""){
-    x=parseFloat(x1)+parseFloat((y-y1)*((x2-x1)/(y2-y1)));
-    x=x.toFixed(3)
-    document.getElementById("x").value= x
-    document.getElementById("result").innerHTML= x  }
-  GetPoints(x1,y1,x2,y2,x,y)
-}
+// function QuadraticInterpolate() {
+//   var x1=document.getElementById("x1").value
+//   var x2=document.getElementById("x2").value
+//   var y1=document.getElementById("y1").value
+//   var y2=document.getElementById("y2").value
+//   var x3=document.getElementById("x3").value
+//   var y3=document.getElementById("y3").value
+//   var x=document.getElementById("x").value
+//   var y=document.getElementById("y").value
+//   if (x1=="" || y1=="" || x2=="" || y2=="" || y3="" ||x3=""){
+//     alert("Enter a values for both known points.");
+//     return
+//   } else if (x=="" && y==""){
+//     alert("Enter a value for the target x or y point.");
+//     return
+//   } else if (x!="" && y!=""){
+//     alert("Only enter a value for the X or Y target value, not both.");
+//     return
+//   } else if (y == ""){
+//     y=parseFloat(y1*(((x-x2)*(x-x3))/((x1-x2)*(x1-x3))))+parseFloat(y2*(((x-x1)*(x-x3))/((x2-x1)*(x2-x3))))+parseFloat(y3*(((x-x1)*(x-x3))/((x3-x1)*(x3-x2))));
+//     y= y.toFixed(3)
+//     document.getElementById("result").innerHTML= y
+//     // document.getElementById("y").value= y
+//   } else if (x == ""){
+//     x=parseFloat(x1*(((y-y2)*(y-y3))/((y1-y2)*(y1-y3))))+parseFloat(x2*(((y-y1)*(y-y3))/((y2-y1)*(y2-y3))))+parseFloat(x3*(((y-y1)*(y-y3))/((y3-y1)*(y3-y2))));
+//     x=x.toFixed(3)
+//     // document.getElementById("x").value= x
+//     document.getElementById("result").innerHTML= x  }
+//   GetPoints(x1,y1,x2,y2,x,y)
+// }
