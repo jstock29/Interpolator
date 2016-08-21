e2cf304f266cbfda7cea2c79ced44ce05536f541
Jared Stock <jstock529@gmail.com>
2016-08-21 15:00:16 -0400

    Some deployment fixes

--- server.js
+++ server.js
@@ -1,10 +1,50 @@
 var http = require('http');
 
-var server = http.createServer(function(req,res){
-res.writeHead(200,{'Content=-Type':'text/html'});
-res.end('<h1>Hello World</h1>');
-});
-
-var port=Number(process.env.PORT || 3000);
-
-server.listen(port);
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("It's alive!");
+  response.end();
+}).listen(3000);
+// 
+//
+// function sendPage(response, filePath, fileContents) {
+//   response.writeHead(200, {"Content-type" : mime.lookup(path.basename(filePath))});
+//   response.end(fileContents);
+// }
+//
+// function send404(response) {
+//   response.writeHead(404, {"Content-type" : "text/plain"});
+//   response.write("Error 404: resource not found");
+//   response.end();
+// }
+//
+// function serverWorking(response, absPath) {
+//   fs.exists(absPath, function(exists) {
+//     if (exists) {
+//       fs.readFile(absPath, function(err, data) {
+//         if (err) {
+//           send404(response)
+//         } else {
+//           sendPage(response, absPath, data);
+//         }
+//       });
+//     } else {
+//       send404(response);
+//     }
+//   });
+// }
+//
+// var server = http.createServer(function(request, response) {
+//   var filePath = false;
+//
+//   if (request.url == '/') {
+//     filePath = "public/index.html";
+//   } else {
+//     filePath = "public" + request.url;
+//   }
+//
+//   var absPath = "./" + filePath;
+//   serverWorking(response, absPath);
+// });
+//
+// var port_number = server.listen(process.env.PORT || 3000);
