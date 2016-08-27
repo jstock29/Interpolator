cc77b8fccfb41ffe449e5b5e5b00beae62dc1821
Jared Stock <jstock529@gmail.com>
2016-08-27 13:27:52 -0400

    Adapt for html deployment

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 var express = require('express');
 var app = express();
+http = require('http'),
+httpServer = http.Server(app);
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
 app.get('/', function(request, response) {
-  response.render(request);
+  response.sendfile(__dirname+'index.html');
 });
 
 app.listen(app.get('port'), function() {
